d425c1354ca244bc829bad3f0edd9715a24e3a61
Ubuntu <ubuntu@ip-172-31-41-23.us-west-2.compute.internal>
2013-07-22 11:46:27 +0000

    Version 3

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var buff = fs.readFileSync("index.html");
+var fs = require('fs');
+var buff = new Buffer(fs.readFileSync("index.html"));
 var app = express.createServer(express.logger());
-var buffer = new Buffer(buff);
-var content = buffer.toString();
+var content = buff.toString();
 
 app.get('/', function(request, response) {
     response.send(content);
